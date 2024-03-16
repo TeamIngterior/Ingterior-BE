4c4ba30d7e35437e0745175aa23d2eeb5462b247
박지훈 <qkrwlgns0510@gmail.com>
2024-03-16 19:54:00 +0900

    OAuth2 로그인 구현 연기

--- src/main/java/com/team_ingterior/ingterior/config/SecurityConfig.java
+++ src/main/java/com/team_ingterior/ingterior/config/SecurityConfig.java
@@ -5,14 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import com.team_ingterior.ingterior.security.CustomOAuth2UserService;
+
+import lombok.RequiredArgsConstructor;
 
 //import com.team_ingterior.ingterior.filter.JwtTokenFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig{
+
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,11 +31,19 @@ public class SecurityConfig{
             .cors(cors -> cors.disable())
             .authorizeHttpRequests(request -> request
                 .anyRequest().permitAll());
-            
+        
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http
+            .oauth2Login(login -> {
+                login.userInfoEndpoint().userService(customOAuth2UserService);
+                //login.failureHandler(null);
+                //login.successHandler(null);
+                });
             
             
                         //.addFilterBefore(new JwtTokenFilter(), UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
         return http.build();	// 어떠한 요청이라도 인증필요
 
-    }   
+    }
+
 }
